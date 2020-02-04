aacd63a04ab5ab61db74fea085ea727bdb8f94eb
Merilin Võrk <merilinv@tlu.ee>
2020-02-04 10:13:39 +0200

    Minu esimene commit

---
+++ w01_funktsioon/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{   // class name alway the same as .java file name
+    static int number = 5; // static helps to use number 
+    public static void main(String[] args){ 
+        //static - belongs to object, 
+        //void - empty main, doesnt return, 
+        //public - [] - array, 
+        //args - just name for the
+        System.out.println("Hello world!");
+    }
+}
