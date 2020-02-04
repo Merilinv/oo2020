7f4f8a44da4f5ffa8b2a2178d30bad9a760ac8a6
Merilin Võrk <merilinv@tlu.ee>
2020-02-04 10:18:04 +0200

    Minu esimene commit

--- w01_funktsioon/HelloWorld.java
+++ w01_funktsioon/HelloWorld.java
@@ -4,7 +4,14 @@ public class HelloWorld{   // class name alway the same as .java file name
         //static - belongs to object, 
         //void - empty main, doesnt return, 
         //public - [] - array, 
-        //args - just name for the
+        //args - String array argument name
         System.out.println("Hello world!");
     }
 }
+
+//git status
+//git add --all
+//git commit -m "Minu esimene commit"
+//git config user.email ...
+//git config usern.name ...
+//git push
